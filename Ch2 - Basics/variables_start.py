e9775689cfaef23d7227a1e341e010cfd56dfd20
Steven Vore <steven@vorefamily.net>
2023-09-06 10:08:42 -0400

    chapter one basics

--- Ch2 - Basics/variables_start.py
+++ Ch2 - Basics/variables_start.py
@@ -1,4 +1,4 @@
-# 
+#
 # Example file for variables
 # LinkedIn Learning Python course by Joe Marini
 #
@@ -13,23 +13,29 @@ mylist = [0, 1, "two", 3.2, False]
 mytuple = (0, 1, 2)
 mydict = {"one" : 1, "two" : 2}
 
-print(myint)
-print(myfloat)
-print(mystr)
-print(mybool)
-print(mylist)
-print(mytuple)
-print(mydict)
+# print(myint)
+# print(myfloat)
+# print(mystr)
+# print(mybool)
+# print(mylist)
+# print(mytuple)
+# print(mydict)
 
 # re-declaring a variable works
 
 # to access a member of a sequence type, use []
+print(mylist[3])
 
 # use slices to get parts of a sequence
+s = slice(5,9)
+print("'"+mystr[s]+"'")
 
 # you can use slices to reverse a sequence
+s = slice(8,4,-1) #why does this not use same indicies?
+print("'"+mystr[s]+"'")
 
 # dictionaries are accessed via keys
+print(mydict["one"])
 
 # ERROR: variables of different types cannot be combined
 
