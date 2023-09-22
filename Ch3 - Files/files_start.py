5354d6b146b36ca53b288d903803559e8fd9cb9c
Steven Vore <steven@vorefamily.net>
2023-09-22 18:53:30 -0400

    write to a file

--- Ch3 - Files/files_start.py
+++ Ch3 - Files/files_start.py
@@ -4,21 +4,24 @@
 #
 
 
-def main():  
+def main():
     # Open a file for writing and create it if it doesn't exist
+    mode = "w+" #write (not append), create if doesn't exist
+    myfile = open("textfile.txt", mode)
 
-    
     # Open the file for appending text to the end
 
 
     # write some lines of data to the file
+    for i in range(10):
+        mystring = "line "+str(i)+"\n"
+        myfile.write(mystring)
 
-    
     # close the file when done
+    myfile.close
 
-    
     # Open the file back up and read the contents
 
-    
+
 if __name__ == "__main__":
     main()
